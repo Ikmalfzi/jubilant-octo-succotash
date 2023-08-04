cb25ecabdb4a716d3b57843fab1d2b498542ebf9
Ikmalfzi <138585243+Ikmalfzi@users.noreply.github.com>
2023-08-04 09:29:15 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+
+
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+
+let banner = document.querySelector('.banner')
+let darkBtn = document.querySelector('#dark')
+let lightBtn = document.querySelector('#light')
+
+
+// ======= Typed Effect=======
+
+let typingText = new Typed('#text', {
+    strings: ["State Islamic University of Sultan Maulana Hasanuddin Banten", "Islamic insurance students","NIM:231430035"],
+    loop: true,
+    typeSpeed: 35,
+    backSpeed: 20,
+    backDelay: 2000,
+})
+
+let myTheme = localStorage.getItem('theme')
+
+if (myTheme) {
+    document.body.className = myTheme
+}
+lightBtn.onclick = () => {
+    document.body.className = 'dark'
+    localStorage.setItem('theme', 'dark')
+}
+darkBtn.onclick = () => {
+    document.body.className = 'light'
+    localStorage.setItem('theme', 'light')
+
+}
+
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
